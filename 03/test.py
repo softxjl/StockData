936e5391079b5fc766b226260bcf033610aec7a5
[softxjl] <[softxjl@126.com]>
2018-05-22 19:38:07 +0800

    12

--- 03/test.py
+++ 03/test.py
@@ -5,29 +5,24 @@
 
 
 import tushare as ts
-import MySQLdb as mdb
+import pandas as pd
+from sqlalchemy import create_engine
 
 
-def insert_snp500_symbols(symbols):
-    db_host = 'localhost'
-    db_user = 'root'
-    db_pass = '123456'
-    db_name = 'stockdata'
-    con = mdb.connect(
-        host=db_host, user=db_user, passwd=db_pass, db=db_name
-    )
-    column_str = """ticker, instrument, name, sector, 
-                 currency, created_date, last_updated_date
-                 """
-    insert_str = ("%s, " * 7)[:-2]
-    final_str = "INSERT INTO symbol (%s) VALUES (%s)" % \
-        (column_str, insert_str)
-    with con:
-        cur = con.cursor()
-        cur.executemany(final_str, symbols)
+engine = create_engine('mysql://root:123456@localhost/tushare?charset=utf8')
 
-stockList=ts.get_stock_basics()
+#stockList=ts.get_stock_basics()
+#try:
+    #stockList.to_sql('tick_data',engine,if_exists='append')#存入数据库，这句有时候运行一次报错，运行第二次就不报错了，不知道为什么
+    #pd.io.sql.to_sql(stockList,'tick_data', engine, schema='tushare', if_exists='append')
+    #df1 = pd.read_sql('tick_data',engine)#从数据库中读取表存为DataFrame
+#except:
+ #   stockList.to_sql('tick_data',engine,if_exists='append')#存入数据库，这句有时候运行一次报错，运行第二次就不报错了，不知道为什么
 
 
-
+df = ts.get_today_all()
+try:
+    df.to_sql('hqlist',engine,if_exists='append')#存入数据库，这句有时候运行一次报错，运行第二次就不报错了，不知道为什么
+except:
+    df.to_sql('hqlist',engine,if_exists='append')#存入数据库，这句有时候运行一次报错，运行第二次就不报错了，不知道为什么
 
